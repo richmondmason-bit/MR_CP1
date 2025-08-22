5541aec0f908144087664765f90fabf957bfa3f0
richmondmason-bit <mason.ricmond@ucas-edu.net>
2025-08-22 09:51:27 -0600

    who_are_you

---
+++ who_are_you.py
@@ -0,0 +1,15 @@
+# Mason R, who_are_you
+
+
+name = input("What is your name?")
+
+age = input ("what is your age?")
+
+color = input("What is your favorite color?")
+
+
+print(name)
+print(age)
+print(color)
+print ("Hello",name,"I'm also ",age,"and my favorite color is red but i also do like",color )
+            
