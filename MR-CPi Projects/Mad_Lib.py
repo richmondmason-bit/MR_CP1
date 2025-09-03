cd044c956407343c63a919fc05b8cf0e9a20bf61
richmondmason-bit <richmond.mason@ucas-edu.net>
2025-09-03 09:51:51 -0600

    hey

--- MR-CPi Projects/Mad_Lib.py
+++ MR-CPi Projects/Mad_Lib.py
@@ -1,4 +1,4 @@
-import random
+
 
 
 name = input("Enter a name: ")
@@ -13,13 +13,8 @@ place = input("Enter a place: ")
 stories = [
     f"One day, {name} went to the {place} and found a {adjective} {animal}. "
     f"Without thinking, {name} decided to {verb} with it. Afterwards, they both ate {food} and became best friends.",
-
-    f"In the middle of the {place}, a {adjective} {animal} stole {name}'s {food}. "
-    f"To get it back, {name} had to {verb} faster than ever before!",
-
-    f"{name} was having a normal day until a {adjective} {animal} appeared at the {place}. "
-    f"It asked for {food} politely, but {name} just wanted to {verb} instead."]
+]
+   
 
 
-mad_lib = random.choice(stories)
 print(mad_lib)
