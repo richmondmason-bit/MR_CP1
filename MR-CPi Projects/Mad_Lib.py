ee9265e07c527d76770561974e8acd3e02336a7b
richmondmason-bit <richmond.mason@ucas-edu.net>
2025-09-04 09:32:41 -0600

    ya

--- MR-CPi Projects/Mad_Lib.py
+++ MR-CPi Projects/Mad_Lib.py
@@ -1,20 +1,14 @@
-
-
-
 name = input("Enter a name: ")
 animal = input("Enter an animal: ")
 food = input("Enter a type of food: ")
 verb = input("Enter a verb: ")
 adjective = input("Enter an adjective: ")
-print("Dont make the place specific")
+print("Don't make the place specific")
 place = input("Enter a place: ")
 
+mad_lib = "One day, " + name + " went to the " + place + " and found a " + adjective + " " + animal + ". " \
+          "Without thinking, " + name + " decided to " + verb + " with it. Afterwards, they both ate " + food + " and became best friends."
 
-stories = [
-    f"One day, {name} went to the {place} and found a {adjective} {animal}. "
-    f"Without thinking, {name} decided to {verb} with it. Afterwards, they both ate {food} and became best friends.",
-]
-   
-
-
+print("\nHere’s your Mad Lib:\n")
 print(mad_lib)
+
---
+++ MR-CPi Projects/string_method_notes.py
@@ -0,0 +1,9 @@
+#string_method note
+
+subject = "Computer Programming 1!"
+
+print(subject.upper().center(100))
+
+color = input("What is your favorite color?").strip()
+print("That is cool. i like" + color + "Too")
+#Stupid/idiot Proofing inputs
