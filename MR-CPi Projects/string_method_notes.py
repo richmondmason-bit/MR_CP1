3036bae187400472ea23a9c363e211eff65775bf
richmondmason-bit <richmond.mason@ucas-edu.net>
2025-09-05 09:28:31 -0600

    ya

--- MR-CPi Projects/string_method_notes.py
+++ MR-CPi Projects/string_method_notes.py
@@ -1,9 +1,20 @@
-#string_method note
+#string_method
 
 subject = "Computer Programming 1!"
 
-print(subject.upper().center(100))
+#print(subject.upper().center(100))
 
-color = input("What is your favorite color?").strip()
-print("That is cool. i like" + color + "Too")
-#Stupid/idiot Proofing inputs
+#color = input("What is your favorite color?").strip().lower()
+#print("That is cool. i like" + color + "Too")
+
+#Stupid/idiot Proofing Inputs
+
+#color = input("What is your favorite color?").strip().lower()
+#print("That is cool. i like" + color + "Too")
+
+word = "jumps"
+sentence = "The quick brown fox jumps over the lazy dog"
+length = len("lazy")
+print(sentence[start:start+length])
+start = sentence.index(word)
+print(sentence.find(word))
