b3c7416ccf3673967272c519e0a46cfd15346a57
richmondmason-bit <richmond.mason@ucas-edu.net>
2025-09-09 09:16:04 -0600

    hey

---
+++ Practice/f string majiggie.py
@@ -0,0 +1,8 @@
+name = "alice"
+age = 25
+height = 1.65
+favorite_color = "blue"
+
+print(f"user info:{name}{age}{height}{favorite_color}")
+print(f"{name} is {age} years old and {height}m tall")
+#print(f"favorite color:{favorite_color}Color code {favorite_color[0]}")
