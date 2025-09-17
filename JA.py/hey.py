c8369a5b89e3796c5804c149931eba61a0f8d7d8
richmondmason-bit <github email>
2025-09-17 09:45:01 -0600

    67

--- JA.py/hey.py
+++
@@ -1,7 +0,0 @@
-name = "jake" 
-age = 67
-money = 67.67
-percent = 0.67
-print("hello{},you are {}. That is so cool. you must have ${:} you're so rich ".format(name,age,money))
-
-print(f"hello {name},you are {age} That is so old! You have {money} You must be rich! random percent is {percent.1f}%")
