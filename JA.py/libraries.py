d068f4539952d9c7e1607cabc0ddb4cc05e61c9d
richmondmason-bit <richmond.mason@ucas-edu.netgit config --global user.email>
2025-11-04 09:54:07 -0700

    hyell

--- JA.py/libraries.py
+++ JA.py/libraries.py
@@ -1,17 +1,11 @@
 import turtle
 import random
 t = turtle.Turtle()
-t.speed(1000)
+t.speed(0)
 t.color("black")
-def draw_and_move():
-    for i in range(100): 
-        for _ in range(100):
-            t.circle(50)
-            t.right(2)  
-        t.penup()
-        new_x = random.randint(-500, 500)
-        new_y = random.randint(-500, 500)
-        t.goto(new_x, new_y)
-        t.pendown()
-draw_and_move()
-turtle.done()
+while True:
+    t.circle(500)
+    t.right(2)  
+    t.penup()   
+    t.pendown()
+else:turtle.done()
